9c49651ad09179722a5e718905730f383751503a
Juliana Gomes <juliana.silvagomes93@gmail.com>
2021-01-20 14:54:38 -0300

    Project created

---
+++ src/test/java/com/aulaspring/projetowebservice/ProjetoWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulaspring.projetowebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

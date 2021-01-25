e62b8c310ec7f6ee9c1e96dfcf0d96722cf2dd0e
Juliana Gomes <juliana.silvagomes93@gmail.com>
2021-01-25 09:47:34 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/aulaspring/projetowebservice/entities/User.java
+++ src/main/java/com/aulaspring/projetowebservice/entities/User.java
@@ -1,13 +1,18 @@
 package com.aulaspring.projetowebservice.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -19,6 +24,11 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	//Essa notação indica que será um User para muitos Pedidos(Order)
+	//Na outra classe ele está mapeado por client
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {}
 
@@ -70,6 +80,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -94,8 +108,7 @@ public class User implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-	
+	}	
 	
 	
 }

39e51bc0c91de85e2db3f521a169747cf1a540b1
Juliana Gomes <juliana.silvagomes93@gmail.com>
2021-02-02 14:44:39 -0300

    User update

--- src/main/java/com/aulaspring/projetowebservice/resources/UserResource.java
+++ src/main/java/com/aulaspring/projetowebservice/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,6 +54,7 @@ public class UserResource {
     	return ResponseEntity.created(uri).body(obj);
     }
     
+    //Para deletar nós usamos o método DELETE
     //Usamos o tipo Void pois não precisamos de nenhum retorno
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id){
@@ -61,4 +63,12 @@ public class UserResource {
     	//Utilizamos o .noContent pois o retorno não possui conteúdo
     	return ResponseEntity.noContent().build(); //Código 204
     }
+    
+    //Para atualizar utilizamos o método PUT
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+    	obj = service.update(id, obj);
+    	return ResponseEntity.ok().body(obj);
+    }
+    
 }
--- src/main/java/com/aulaspring/projetowebservice/service/UserService.java
+++ src/main/java/com/aulaspring/projetowebservice/service/UserService.java
@@ -15,30 +15,43 @@ import com.aulaspring.projetowebservice.repositories.UserRepository;
 //Aqui vamos utilizar o @Service pois nossa classe é um serviço
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
-		//Para recuperar um usuário pelo id
-	 	Optional<User> obj = repository.findById(id);
-	 	
-	 	//O get pega o objeto que está dentro do Optional
-	 	return obj.get();
+		// Para recuperar um usuário pelo id
+		Optional<User> obj = repository.findById(id);
+
+		// O get pega o objeto que está dentro do Optional
+		return obj.get();
 	}
-	
-	//Salvar usuário no banco de dados
+
+	// Salvar usuário no banco de dados
 	public User insert(User obj) {
-		return repository.save(obj); //O save já retorna o objeto salvo
+		return repository.save(obj); // O save já retorna o objeto salvo
 	}
-	
-	//Deletar usuário no banco de dados
+
+	// Deletar usuário no banco de dados
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
-	
+
+	//Atualizar usuário no banco de dados
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id); //O getOne vai instânciar um usuário
+		updateData(entity, obj); //Método que irá atualizar o usuário
+		return repository.save(entity); //Salvamos a alteração no banco de dados
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
+
 }
